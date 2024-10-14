1517d935d8281bd1b742c58c1f8976116ca846f6
Max <1747697@uab.cat>
2024-10-14 10:20:53 +0200

    Paso 4 correcto pero lento

---
+++ P-1/script_pas1234.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cut -d "," --complement -f 12,16 < supervivents.csv > sortidapas1.csv
+
+lin1=$(wc -l < sortidapas1.csv)
+awk -F',' '{if($14 == "False" || NR == 1) print $0}' sortidapas1.csv > sortidapas2.csv
+lin2=$(wc -l < sortidapas2.csv)
+eliminats=$(($lin1 - $lin2))
+echo "Número de línies esborrades:" $eliminats
+
+awk -F',' 'NR == 1 {print $0 ",Ranking_views"}
+NR != 1 {
+    if ($8 < 1000000) {
+        missatge = ",Bo"
+    } else if ($8 >= 1000000 && $8 <= 10000000) {
+        missatge = ",Excel·lent"
+    } else {
+        missatge = ",Estrella"
+    }
+    print $0 missatge
+}' sortidapas2.csv > sortidapas3.csv
+
+count=0
+while read linia; do
+
+    if [ $count -eq 0 ]; then
+        echo "$linia,Rlikes,Rdislikes" > sortida.csv
+        count=$((count+1))
+    else
+        likes=$(cut -d "," -f 9 <<< "$linia")
+        dislikes=$(cut -d "," -f 10 <<< "$linia")
+        views=$(cut -d "," -f 8 <<< "$linia")
+        if [ $views -eq 0 ]; then
+            rlikes=0
+            rdislikes=0
+        else
+            rlikes=$((likes*100/views))
+            rdislikes=$((dislikes*100/views))
+        fi
+        echo "$linia,$rlikes,$rdislikes" >> sortida.csv
+    fi
+done < sortidapas3.csv
+
+    
